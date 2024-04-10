ae1e0333591ee81ff48af4e258bd343bb0884e76
Ryan Mijares <166569891+mijaresryan0502@users.noreply.github.com>
2024-04-09 21:36:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+def Decode(input):
+  new_pass = ''
+  for i in range(len(input)):
+    if input[i] <= 2:
+      new_pass += input[i] + 7
+    else:
+      new_pass += input[i] - 3
+  return [new_pass, input]
